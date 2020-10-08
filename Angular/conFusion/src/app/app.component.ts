57ddbf345ffa321ade3a64ee04a1616391aae591
Yomna-Anwer <yoya25397@gmail.com>
2020-10-08 15:34:46 +0200

    Task1 and task2 are done (Creating a new component and view the left part of it)

--- Angular/conFusion/src/app/app.component.ts
+++ Angular/conFusion/src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+//this provides us with the ability to define a component
+//So, this will define one component that forms part of our Angular application. 
+import { Component } from '@angular/core'; 
 
+// this is a component decorator
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // make it able to use as a tag in the HTML file
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
